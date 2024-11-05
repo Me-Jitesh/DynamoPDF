f7e0ee3006a4194d0ee14e343672543be7ef08e9
Jitesh Singh <jiteshs101@gmail.com>
2024-11-05 08:03:55 +0530

    Model finished

---
+++ src/main/java/com/jitesh/dynamopdf/DynamoPdfApplication.java
@@ -0,0 +1,13 @@
+package com.jitesh.dynamopdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamoPdfApplication.class, args);
+    }
+
+}
